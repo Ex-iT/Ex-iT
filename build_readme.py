5801d2eaec53b006ffe092b2fdc4ae995de0959d
Jeroen D <Ex-iT@users.noreply.github.com>
2021-06-27 12:05:17 +0200

    Added TryHackMe in favor of Twitter and BitBucket

--- build_readme.py
+++ build_readme.py
@@ -14,16 +14,8 @@ params = { 'per_page': '10' }
 content = '''<table>
     <tr>
         <td>
-            <a href="https://twitter.com/jdijkman">
-                <img alt="Profile for Ex-iT at Twitter" src="https://68ef2f69c7787d4078ac-7864ae55ba174c40683f10ab811d9167.ssl.cf1.rackcdn.com/twitter-icon_64x64.png" />
-            </a>
-            <a href="https://bitbucket.org/jdijkman/">
-                <img align="right" alt="Profile of JDijkman at Bitbucket" src="https://icons.iconarchive.com/icons/limav/flat-gradient-social/64/Bitbucket-icon.png" />
-            </a>
-        </td>
-        <td>
-            <a href="https://steamcommunity.com/id/Ex-iT">
-                <img alt="Profile for Ex-iT at Steam" src="https://steamcommunity-a.akamaihd.net/public/shared/images/header/globalheader_logo.png" />
+            <a href="https://stackoverflow.com/users/3351720/ex-it">
+                <img alt="Profile for Ex-iT at Stack Overflow, Q&amp;A for professional and enthusiast programmers" src="https://stackoverflow.com/users/flair/3351720.png?theme=dark" />
             </a>
         </td>
         <td rowspan="2">
@@ -34,13 +26,13 @@ content = '''<table>
     </tr>
     <tr>
         <td>
-            <a href="https://stackoverflow.com/users/3351720/ex-it">
-                <img alt="Profile for Ex-iT at Stack Overflow, Q&amp;A for professional and enthusiast programmers" src="https://stackoverflow.com/users/flair/3351720.png?theme=dark" />
+            <a href="https://app.hackthebox.eu/profile/169430">
+                <img alt="Hack The Box :: MrBlonde" src="https://www.hackthebox.eu/badge/image/169430" />
             </a>
         </td>
         <td>
-            <a href="https://app.hackthebox.eu/profile/169430">
-                <img alt="Hack The Box :: MrBlonde" src="https://www.hackthebox.eu/badge/image/169430" />
+            <a href="https://tryhackme.com/p/MrBlonde/">
+                <img alt="TryHackMe :: MrBlonde" src="https://tryhackme-badges.s3.amazonaws.com/MrBlonde.png" />
             </a>
         </td>
     </tr>
