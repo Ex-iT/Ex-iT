e42ccfd95415b0d2344adbfadeab4e3d775421fe
Ex-iT <vic.toothpick@gmail.com>
2024-04-06 00:08:55 +0200

    Updated activity log with commits grouped by repo

--- build_readme.py
+++ build_readme.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import requests
 import json
 from datetime import datetime
-import string
 
 file_path = Path(__file__).parent.resolve() / 'README.md'
 user = 'Ex-iT'
@@ -47,24 +46,35 @@ content = '''<table>
 
 <pre>'''
 
+def pushMessage(event):
+    for event in (event for event in json_data if event['type'] == 'PushEvent'):
+        created_at = datetime.strptime(event['created_at'], '%Y-%m-%dT%H:%M:%SZ')
+        formatted_date = created_at.strftime('%d-%m-%Y')
+        repo_name = event['repo']['name'];
+        repo_label = repo_name.replace(f'{user}/', '')
+        repo_url = f'{main_url}/{repo_name}'
+
+        commit_texts = f'''┌──[{formatted_date}]─[<a href="{repo_url}">{repo_label}</a>]'''
+
+        for commit in event['payload']['commits']:
+            commit_texts += commitMessage(commit, repo_name)
+
+        return commit_texts + '<br /><br />'
+
+def commitMessage(commit, repo_name):
+    commit_url = f'''{main_url}/{repo_name}/commit/{commit['sha']}'''
+    commit_message = commit['message']
+
+    return f'''
+└───■ <a href="{commit_url}">{commit_message}</a>'''
+
+
 if __name__ == '__main__':
     response = requests.get(f'{api_url}/users/{user}/events/public', headers=headers, params=params)
     json_data = json.loads(response.text)
 
-    push_events = (event for event in json_data if event['type'] == 'PushEvent')
-    for event in push_events:
-        created_at = datetime.strptime(event['created_at'], '%Y-%m-%dT%H:%M:%SZ')
-        formatted_date = created_at.strftime('%d-%m-%Y')
-        repo_name = event['repo']['name'];
-        repo_label = string.capwords(repo_name.replace(f'{user}/', '').replace('-', ' ')) # Lazy way for a pretty repo name
-        repo_url = f'{main_url}/{repo_name}'
-        commit_message = event['payload']['commits'][0]['message'] # Always take the first message
-        commit_sha = event['payload']['commits'][0]['sha']
-        commit_url = f'{main_url}/{repo_name}/commit/{commit_sha}'
-
-        content += f'''
-┌──[{formatted_date}]─[<a href="{repo_url}">{repo_label}</a>]
-└───■ <a href="{commit_url}">{commit_message}</a><br />'''
+    for event in (event for event in json_data if event['type'] == 'PushEvent'):
+        content += pushMessage(event)
 
     content += '''
 </pre>'''
